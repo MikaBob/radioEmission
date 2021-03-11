7a46a24188e2172586a857117e04b5117b45d5f9
MikaBob <pji-2bke@hotmail.fr>
2021-03-11 20:35:28 +0100

    add infrared command for button_3. Set ON/OFF param as mandatory (default OFF)

--- switch.php
+++ switch.php
@@ -1,19 +1,17 @@
 <?php
 $INTERRUPTORS = [
 	"BUTTON_1" => "12345665",
-	"BUTTON_2" => "12345664",
-	"BUTTON_3" => "12345666" 
+        "BUTTON_2" => "12345664",
+        "BUTTON_3" => "12345666"
 ];
 
 if(isset($_GET["interruptor"])) {
 	foreach( $INTERRUPTORS as $interruptorName => $interruptorId){
 		if( in_array($interruptorName, explode(";", $_GET["interruptor"], count($INTERRUPTORS)))) {
-			if( !file_exists($interruptorName) ){
-				system('./radioEmission 7 '.$interruptorId.' 1 on');
-				fopen($interruptorName, "w");
+			if( isset($_GET["action"]) && $_GET["action"] === "ON" ){
+				system('sudo ./switch.sh '.$interruptorName.' ON');
 			} else {
-				system('./radioEmission 7 '.$interruptorId.' 1 off');
-				unlink($interruptorName);
+				system('sudo ./switch.sh '.$interruptorName.' OFF');
 			}
 		}
 	}
--- switch.sh
+++ switch.sh
@@ -1,83 +1,90 @@
 #!/bin/bash
 
+# HOW TO USE:
+# ./switch.sh SWITCH_ALL ON
+# ./switch.sh BUTTON_1 OFF
+
 BUTTON_1=12345665
 BUTTON_2=12345664
 BUTTON_3=12345666
 
 STRING_BUTTON_1="BUTTON_1"
 STRING_BUTTON_2="BUTTON_2"
-STRING_BUTTON_3="BUTTON_3"
+STRING_BUTTON_3="BUTTON_3" 
 STRING_SWITCH_ALL="SWITCH_ALL"
 STRING_ON="ON"
 
+CURRENT_DIR="/var/www/html/radioEmission/"
+
 now=$(date)
+
+sendRadioCommand () {
+        sudo /var/www/html/radioEmission/radioEmission 7 $1 1 $2
+}
+
 echo -e "\n"
 echo $now
-
 echo $1
 echo $2
 
-if [[ $1 = $STRING_BUTTON_1 ]];
-then
-    if [[ -f $1 ]];
+if [[ $1 = $STRING_BUTTON_1 ]]; then
+    if [[ $2 = $STRING_ON ]];
     then
-        echo "Switching OFF #1"
-        /var/www/html/radioEmission/radioEmission 7 $BUTTON_1 1 off
-        rm $STRING_BUTTON_1
-    else
         echo "Switching ON #1"
-        /var/www/html/radioEmission/radioEmission 7 $BUTTON_1 1 on
-        touch $STRING_BUTTON_1
+        sendRadioCommand $BUTTON_1 on
+        sudo echo " " > $CURRENT_DIR$STRING_BUTTON_1
+    else
+        echo "Switching OFF #1"
+        sendRadioCommand $BUTTON_1 off
+        sudo rm $CURRENT_DIR$STRING_BUTTON_1
     fi
 fi
 
-if [[ $1 = $STRING_BUTTON_2 ]];
-then
-        if [[ -f $1 ]];
-        then
-        echo "Switching OFF #2"
-                /var/www/html/radioEmission/radioEmission 7 $BUTTON_2 1 off
-                rm $STRING_BUTTON_2
-        else
-        echo "Switching ON #2"
-                /var/www/html/radioEmission/radioEmission 7 $BUTTON_2 1 on
-                touch $STRING_BUTTON_2
-        fi
+if [[ $1 = $STRING_BUTTON_2 ]]; then
+	if [[ $2 = $STRING_ON ]];
+	then
+		echo "Switching ON #2"
+		sendRadioCommand $BUTTON_2 on
+		sudo echo " " > $CURRENT_DIR$STRING_BUTTON_2
+	else
+		echo "Switching OFF #2"
+		sendRadioCommand $BUTTON_2 off
+		sudo rm $CURRENT_DIR$STRING_BUTTON_2
+	fi
 fi
 
-if [[ $1 = $STRING_BUTTON_3 ]];
-then
-        if [[ -f $1 ]];
-        then
-        echo "Switching OFF #3"
-        /var/www/html/radioEmission/radioEmission 7 $BUTTON_3 1 off
-        rm $STRING_BUTTON_3
-        else
-        echo "Switching ON #3"
-        /var/www/html/radioEmission/radioEmission 7 $BUTTON_3 1 on
-        touch $STRING_BUTTON_3
-        fi
+if [[ $1 = $STRING_BUTTON_3 ]]; then
+	if [[ $2 = $STRING_ON ]];
+	then
+		echo "Switching ON #3"
+		sendRadioCommand $BUTTON_3 on
+		sudo echo " " > $CURRENT_DIR$STRING_BUTTON_3
+		sudo python /var/www/python/infrared/irrp.py -g16 -p -f /var/www/python/infrared/ir-galaxy modeminus
+	else
+		echo "Switching OFF #3"
+		sendRadioCommand $BUTTON_3 off
+		sudo rm $CURRENT_DIR$STRING_BUTTON_3
+	fi
 fi
 
-if [[ $1 = $STRING_SWITCH_ALL ]];
-then
-    if [[ $2 =  $STRING_ON ]];
+if [[ $1 = $STRING_SWITCH_ALL ]]; then
+    if [[ $2 = $STRING_ON ]];
     then
         echo "Switching ON everything"
-        /var/www/html/radioEmission/radioEmission 7 $BUTTON_1 1 on
-        /var/www/html/radioEmission/radioEmission 7 $BUTTON_2 1 on
-        /var/www/html/radioEmission/radioEmission 7 $BUTTON_3 1 on
-        touch $STRING_BUTTON_1
-        touch $STRING_BUTTON_2
-        touch $STRING_BUTTON_3
+        sendRadioCommand $BUTTON_1 on
+        sendRadioCommand $BUTTON_2 on
+        sendRadioCommand $BUTTON_3 on
+        sudo echo " " > $CURRENT_DIR$STRING_BUTTON_1
+        sudo echo " " > $CURRENT_DIR$STRING_BUTTON_2
+        sudo echo " " > $CURRENT_DIR$STRING_BUTTON_3
+	sudo python /var/www/python/infrared/irrp.py -g16 -p -f /var/www/python/infrared/ir-galaxy modeminus
     else
         echo "Switching OFF everything"
-        /var/www/html/radioEmission/radioEmission 7 $BUTTON_1 1 off
-        /var/www/html/radioEmission/radioEmission 7 $BUTTON_2 1 off
-        /var/www/html/radioEmission/radioEmission 7 $BUTTON_3 1 off
-        rm $STRING_BUTTON_1
-        rm $STRING_BUTTON_2
-        rm $STRING_BUTTON_3
+        sendRadioCommand $BUTTON_1 off
+        sendRadioCommand $BUTTON_2 off
+        sendRadioCommand $BUTTON_3 off
+        sudo rm $CURRENT_DIR$STRING_BUTTON_1
+        sudo rm $CURRENT_DIR$STRING_BUTTON_2
+        sudo rm $CURRENT_DIR$STRING_BUTTON_3
     fi
 fi
-
